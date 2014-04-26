f1f4f618bb2608971fd206d6740181d6bdccc598
spezi77 <spezialitz@gmx.de>
2014-04-26 15:37:37 +0200

    Alignments for building OmniRom 4.4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -74,7 +74,10 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 LEGACY_RIL := true
 BOARD_USES_LEGACY_RIL := true
 
+# Set device resolution for OmniRom
+DEVICE_RESOLUTION := 480x800
+
 # Override in the CM way
 # Use old kernel toolchain.  GCC 4.6 may be the root cause of our modem
 # interface dying all the time.
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.8.3
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7
