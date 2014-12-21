38f82333e7e811dcba31ca4cd0e7377b5b7d1c28
spezi77 <spezi7713@gmail.com>
2014-12-21 14:37:50 +0100

    Fix problem with missing libart.so This partially reverts commit: https://github.com/spezi77/android_device_htc_bravo/commit/9379de402dcca639a6dac8c600fec14b5f49e359
    Change-Id: Ia6d4b6720f9a17c0e10212088892274a17ad6bf9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,8 +93,5 @@ include device/qcom/sepolicy/sepolicy.mk
 LEGACY_RIL := true
 BOARD_USES_LEGACY_RIL := true
 
-# Set for Android 5.0 PA to solve "LIBART_IMG_HOST_BASE_ADDRESS"
-ANDROID_COMMON_BUILD_MK := true
-
 # Override in the CM way
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.8
