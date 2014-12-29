d32a746dc9b86f4cac193a0d481165b3c280e7a4
spezi77 <spezi7713@gmail.com>
2014-12-29 13:30:50 +0100

    Bravo: add SE Policy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,7 +88,36 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE :=  362144000 # 0x09100000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += \
+        device/htc/bravo/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+	bluetooth.te \
+	radio.te \
+	file_contexts \
+	te_macros \
+	device.te \
+	dhcp.te \
+	domain.te \
+	file.te \
+	init.te \
+	kickstart.te \
+	mediaserver.te \
+	netmgrd.te \
+	qmiproxy.te \
+	qmuxd.te \
+	rild.te \
+	secril.te \
+	servicemanager.te \
+	sysinit.te \
+	system.te \
+	system_server.te \
+	time_daemon.te \
+	ueventd.te \
+	vold.te \
+	wpa.te \
+	wpa_supplicant.te \
+	zygote.te
 
 # Radio
 BOARD_PROVIDES_LIBRIL := true
