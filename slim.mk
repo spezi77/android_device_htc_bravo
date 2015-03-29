3ff01083da3d93828f6d82f503771a4db05c3b75
spezi77 <spezi7713@gmail.com>
2015-03-29 18:51:59 +0200

    Get ready for Slim LP
    Change-Id: If9df4240fb2db1ca9a6d42d032f371fc3a86b957

---
+++ slim.mk
@@ -0,0 +1,20 @@
+# Pull all dictionaries (This makes sure it gets called)
+TARGET_USE_KEYBOARD := international
+
+# Call this first so apn list is actually copied
+$(call inherit-product, vendor/slim/config/gsm.mk)
+
+$(call inherit-product, device/htc/bravo/full_bravo.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME    := slim_bravo
+PRODUCT_BRAND   := htc_wwe
+PRODUCT_DEVICE  := bravo
+PRODUCT_MODEL   := HTC Desire
+PRODUCT_MANUFACTURER := HTC
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_bravo BUILD_FINGERPRINT=htc_wwe/htc_bravo/bravo:2.3.3/GRI40/96875.1:user/release-keys TARGET_BUILD_TYPE=userdebug BUILD_VERSION_TAGS=release-keys PRIVATE_BUILD_DESC="3.14.405.1 CL96875 release-keys"
