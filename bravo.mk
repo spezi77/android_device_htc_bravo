a6cb4e23b6d98ec63d0d269183bff0df6ea5b762
spezi77 <spezi77@gmx.com>
2015-08-09 21:21:42 +0200

    Choose charging as default * fixes overlaoding issue with legacy devices * Thanks maniacx: https://github.com/maniacx/android_device_htc_leo/commit/53599ee2ff61cc6131eb8cfc7a9ffb2c24511451
    Change-Id: Ibf48407e0084f3fbbf39ff788977088330f17770

--- bravo.mk
+++ bravo.mk
@@ -71,7 +71,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Set usb type
 ADDITIONAL_DEFAULT_PROPERTIES += \
     persist.sys.ums=1 \
-    persist.sys.usb.config=mass_storage \
+    persist.sys.usb.config=charging,adb \
     persist.service.adb.enable=1
 
 #
