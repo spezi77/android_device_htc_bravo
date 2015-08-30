c7d1a857f48d2939a3cf5492ccadaa79cc322ce5
spezi77 <spezi77@gmx.com>
2015-08-30 23:57:43 +0200

    Remove some more packages
    Change-Id: I088bfd21fbf7bfcc13f7a09749874674f414c5db

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,6 +84,7 @@ EXTENDED_FONT_FOOTPRINT := true
 # Remove packages
 REMOVE_PRODUCT_PACKAGES += Gallery2 Exchange2 LiveWallpapers Galaxy4
 REMOVE_PRODUCT_PACKAGES += Email VisualizationWallpapers LiveWallpapersPicker HoloSpiralWallpaper
+REMOVE_PRODUCT_PACKAGES += BasicDreams NoiseField PhaseBeam
 
 # Bootanimation
 TARGET_BOOTANIMATION_PRELOAD := true
